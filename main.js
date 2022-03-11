d4de30cc7cd161762c72a2b91908944eff15c6cc
nprezhdo <nprezhdo@gmail.com>
2022-03-11 09:12:32 -0800

    Added switch page function

--- main.js
+++ main.js
@@ -10,3 +10,14 @@ window.requestAnimationFrame(draw);
 };
 
 draw();
+
+
+function switchPage(id){
+  if(onButton()){//check jo's method // document.addEventListener('keydown', switchPage(id)); 
+    ctx.clearRect(0,0, canvas.width, canvas.height);
+    var myImg = document.getElementById(id);
+    ctx.drawImage(myImg,0,0,1200,800);
+    window.requestAnimationFrame(draw);
+  }
+
+}
